ee40fc2df7104fc194a9cf1f1b70cc0d5102698a
Sam Yakovlev <bighairyyak@gmail.com>
2017-11-19 13:12:42 -0500

    Initial Commit

--- main.go
+++ main.go
@@ -1,9 +1,19 @@
 package main
 
-import "fmt"
-
+import (
+		"fmt"
+		"net/http"
+)
 
 func main() {
 	fmt.Println("Hello world");
+	http.HandleFunc("/control", handler) //Will respond only to whatever the pattern is AFTER THIS DEVICE'S IP
+	http.ListenAndServe(":8000", nil)
 
+}
+
+func handler(w http.ResponseWriter, r *http.Request){
+
+	r.Form.Get("asdfa")	//Will interpret ?[PARAM] as input parameters and pull them from it
+	fmt.Fprintf(w, "Test message")
 }
