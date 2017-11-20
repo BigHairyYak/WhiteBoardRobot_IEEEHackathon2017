d9d65c3917b0ff5d7fd39d35ad3ec79a4b4615b0
Jesse Stevenson <jessedusty@gmail.com>
2017-11-19 22:23:35 -0500

    Made changes - stuff working now

---
+++ arduino.go
@@ -0,0 +1,84 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"math"
+)
+
+func moveToCoordinate(x, y float64) {
+	initialBoardOffset := 29.5
+	moveToAbstractCoordinate(x, y+initialBoardOffset)
+}
+
+func moveToAbstractCoordinate(x, y float64) {
+	var a, b float64
+	lineOffset := 3.25
+	WidthOfPoints := 47.5
+
+	a = math.Sqrt(math.Pow(-lineOffset/2+x, 2) + math.Pow(y, 2))
+	b = math.Sqrt(math.Pow(-lineOffset/2+WidthOfPoints-x, 2) + math.Pow(y, 2))
+	moveMotorsAbsolute(a, b)
+}
+
+func moveMotorsAbsolute(aInches, bInches float64) {
+	StartingA := 58.5
+	StartingB := 58.0
+
+	newA := StartingA - aInches
+	newB := StartingB - bInches
+
+	moveMotorsInches(newA, newB)
+}
+
+func moveMotorsInches(aInches, bInches float64) {
+	aRev := -1.0
+	bRev := 1.0
+	fmt.Printf("Inches %f %f\n", aInches, bInches)
+	ticksPerInch := float64(136)
+	moveTicks(int(aInches*ticksPerInch*aRev), int(bInches*ticksPerInch*bRev))
+}
+
+func moveTicks(aTicks, bTicks int) {
+
+	fmt.Printf("Ticks %d %d\n", aTicks, bTicks)
+
+	var posXH, posXL = uint8(aTicks >> 8), uint8(aTicks & 0xff) //Split aTicks into upper and lower bytes
+	var posYH, posYL = uint8(bTicks >> 8), uint8(bTicks & 0xff) //Split bTicks into upper and lower bytes
+
+	var instruction []byte //Sets up byte array instruction
+
+	instruction = []byte{byte(1),
+		byte(posXH), byte(posXL),
+		byte(posYH), byte(posYL)}
+
+	sendCommand(instruction)
+
+}
+
+func sendCommand(commandList []byte) {
+
+	/**
+	COMMAND LIST:
+	0 -> MOVE
+	1 -> DISABLE
+	2 -> HOME
+	*/
+
+	log.Printf("%v\n", commandList)
+
+	reader.Reset(s)
+
+	n, err := s.Write(commandList)
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Printf("Sent %v bytes\n", n)
+
+	for i := 0; i < 10; i++ {
+		b, _ := reader.ReadByte()
+		fmt.Printf("Read a %d back\b", b)
+
+	}
+
+}
--- main.go
+++ main.go
@@ -1,17 +1,17 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
+	"github.com/tarm/serial"
 	"log"
 	"net/http"
 	"strconv"
-	//"github.com/tarm/serial"
-	"bufio"
-	"go.bug.st/serial.v1"
-	"os"
+	"time"
 )
 
-var s serial.Port
+var s *serial.Port
+
 var reader *bufio.Reader
 
 func main() {
@@ -35,95 +35,57 @@ func main() {
 	//}
 	//log.Printf("%q", buf[:n])
 
-	ports, err := serial.GetPortsList()
+	c := &serial.Config{Name: "/dev/tty.usbmodem1411", Baud: 9600}
+	var err error
+
+	s, err = serial.OpenPort(c)
 	if err != nil {
 		log.Fatal(err)
 	}
-	if len(ports) == 0 { //If no ports are found
-		log.Fatal("No serial ports found")
-	}
-	for _, port := range ports {
-		fmt.Printf("Found port: %v\n", port) //Outputs "found port [PORT]" per port on a new line
-	}
-	mode := &serial.Mode{BaudRate: 9600}
-	s, err = serial.Open("/dev/ttyACM0", mode)
-	if err != nil {
-		log.Fatal(err)
-	}
+	time.Sleep(time.Second)
 
 	reader = bufio.NewReader(s)
-	http.HandleFunc("/control", handler) //Will respond only to whatever the pattern is AFTER THIS DEVICE'S IP
+
+	//control(1, -1000, -1000)
+	//moveMotorsInches(0, 10)
+	//moveToCoordinate(24, 61.75)
+
+	http.HandleFunc("/control", htmlHandler) //Will respond only to whatever the pattern is AFTER THIS DEVICE'S IP
 	http.HandleFunc("/", positionResponse)
 	http.ListenAndServe(":8000", nil)
 }
 func positionResponse(w http.ResponseWriter, r *http.Request) {
 	fmt.Println("method:", r.Method)
-	r.ParseForm()
-	xposstr := r.PostFormValue("x-pos")
-	yposstr := r.PostFormValue("y-pos")
-	xpos := uint64(0)
-	ypos := uint64(0)
-	var err error
-	if xposstr != "" {
-		xpos, err = strconv.ParseUint(xposstr, 10, 16)
-		if err != nil {
-			log.Fatal(err)
+	if r.Method == "POST" {
+
+		r.ParseForm()
+		xposstr := r.PostFormValue("x-pos")
+		yposstr := r.PostFormValue("y-pos")
+		xpos := float64(0)
+		ypos := float64(0)
+		var err error
+		if xposstr != "" {
+			xpos, err = strconv.ParseFloat(xposstr, 64)
+			if err != nil {
+				log.Fatal(err)
+			}
 		}
-	}
-	if yposstr != "" {
-		ypos, err = strconv.ParseUint(yposstr, 10, 16)
-		if err != nil {
-			log.Fatal(err)
+		if yposstr != "" {
+			ypos, err = strconv.ParseFloat(yposstr, 64)
+			if err != nil {
+				log.Fatal(err)
+			}
 		}
+		fmt.Println("x position = ", xpos)
+		fmt.Println("y position = ", ypos)
+		htmlHandler(w, r)
+		moveToCoordinate(float64(xpos), float64(ypos))
+	} else {
+		htmlHandler(w, r)
 	}
-	fmt.Println("x position = ", xpos)
-	fmt.Println("y position = ", ypos)
-	handler(w, r)
-	control(0, int(xpos), int(ypos))
-}
-func control(command, posX, posY int) { //Putting data type after all variable names sets them all
-	//instruction := {command, posX, posY} THIS IS BAD YOU ARE WRONG
-	var posXH, posXL = uint8(posX >> 8), uint8(posX & 0xff) //Split posX into upper and lower bytes
-	var posYH, posYL = uint8(posY >> 8), uint8(posY & 0xff) //Split posY into upper and lower bytes
-
-	var instruction []byte //Sets up byte array instruction
-	/**
-	COMMAND LIST:
-	0 -> MOVE
-	1 -> DISABLE
-	2 -> HOME
-	*/
-
-	switch command {
-	case 0:
-		instruction = []byte{byte(command),
-			byte(posXH), byte(posXL),
-			byte(posYH), byte(posYL)}
-	case 1:
-		instruction = []byte{byte(1)} //disable
-	case 2:
-		instruction = []byte{byte(2)}
-	default:
-		//cry
-	}
-	fmt.Printf("%v\n", instruction)
-	if s == nil {
-		log.Fatal("S is not defined")
-	}
-	n, err := s.Write(instruction)
-	if err != nil {
-		log.Fatal(err)
-	}
-	fmt.Printf("Sent %v bytes\n", n)
-
-	for true {
-		reader.WriteTo(os.Stdout)
-	}
-
-	reader.ReadByte() //Reads confirmation byte from Arduino, if no byte returned return error
 }
 
-func handler(w http.ResponseWriter, r *http.Request) {
+func htmlHandler(w http.ResponseWriter, r *http.Request) {
 	http.ServeFile(w, r, "./index.html")
 	//control(0, 100, 100)
 	//r.Form.Get("asdfa") //Will interpret ?[PARAM] as input parameters and pull them from it
